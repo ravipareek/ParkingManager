75e0895f9fcd3dfef7e68f7dd54804e793376d54
Pareek Ravi <pareek96@gmail.com>
2018-12-06 13:21:35 -0500

    removed aws keys from code

--- useAws.php
+++ useAws.php
@@ -1,22 +1,28 @@
 <?php
 	use Aws\S3\S3Client;
+	use Aws\Credentials\CredentialProvider;
 	use Aws\S3\Exception\S3Exception;
+	use Aws\Exception\AwsException;
 
 
+
+	// error_reporting(E_ALL);
+ //  	ini_set('display_errors', 1);
+
+  	putenv('HOME=/var/www/4ww3');
 	require 'vendor/autoload.php';
-	$config = require('config.php');
-	// echo "Init AWS";
+	// $config = require('config.php');
+	// echo "Init AWS 		";
+
+
 	try{
 		$s3 = S3Client::factory(array(
 			'region' => 'us-east-2',
 			'version' => 'latest',
-			'credentials' => array(
-				'key' => 'AKIAIIVMFZGFDBJWHYUQ',
-				'secret' => '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'
-			)
+			'profile' => 'default'
 		));
 	}catch(Exception $e){
 		echo $e;
 	}
-	// echo "S3 ";
+	// echo "S3";
 ?>
